784eb2c88cdb1c8293f3037b0af317fb1fc5eb8a
erickgodinho <godinho.erickc@gmail.com>
2024-09-12 00:38:04 -0300

    initial commit

--- src/Desafio/Produto.java
+++ src/Desafio/Produto.java
@@ -1,4 +1,25 @@
 package Desafio;
 
 public class Produto {
+    String nome;
+    double preco;
+    int quantidade;
+
+    public Produto(String nome, int quantidade, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+    public double getValorEstoque(){
+        return quantidade * preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preço=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
 }
--- src/Desafio/SuperMarket.java
+++ src/Desafio/SuperMarket.java
@@ -1,4 +1,59 @@
 package Desafio;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class SuperMarket {
+    static ArrayList<Produto> produtos = new ArrayList<>();
+    public static void main(String[] args) {
+
+        boolean continuar = true;
+
+        while(continuar) {
+
+            String menu = "Escolha uma opção:\n" +
+                    "1. Adicionar produtos\n" +
+                    "2. Listar produtos\n" +
+                    " 3. Remover produto\n" +
+                    "4. Calcular total em estoque\n" +
+                    "5. Sair";
+
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog(menu));
+
+            switch (opcao){
+
+                case 1:
+                    adicionarProduto();
+                    break;
+
+                case 2:
+                    listarProdutos();
+                    break;
+
+                case 3:
+                    removerProdutos();
+                    break;
+
+                case 4:
+                    calcularTotalEstoque;
+                    break;
+
+                case 5:
+                    continuar = false;
+                    JOptionPane.showMessageDialog(null,"Saindo...");
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção invalida");
+
+
+            }
+
+        }
+
+    }
+
+    public static void adicionarProduto();
+
 }
+
